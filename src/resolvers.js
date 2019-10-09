73564146ac36a7333dbd0354196b913d4e92f556
creisle <caralynreisle@gmail.com>
2019-10-09 09:15:45 -0700

    Add get by ID resolvers

--- src/resolvers.js
+++ src/resolvers.js
@@ -18,6 +18,24 @@ const resolvers = {
             session.close();
             return result;
         },
+        getStudy: async (root, {id}, {db}) =>  {
+            const session = await db.session();
+            const [result] = await session.query('Studies').where({id}).all();
+            session.close();
+            return result;
+        },
+        getPatient: async (root, {id}, {db}) =>  {
+            const session = await db.session();
+            const [result] = await session.query('Patients').where({id}).all();
+            session.close();
+            return result;
+        },
+        getBiopsy: async (root, {id}, {db}) =>  {
+            const session = await db.session();
+            const [result] = await session.query('Biopsies').where({id}).all();
+            session.close();
+            return result;
+        }
     },
 }
 
