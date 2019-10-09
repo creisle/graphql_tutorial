21c2bb66c5f950ae28712aff305c5ee4d9847668
creisle <caralynreisle@gmail.com>
2019-10-09 09:17:34 -0700

    Add FK resolvers

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,4 +1,35 @@
 const resolvers = {
+    Patient: {
+        // This resolves the FK relationship between patient and study
+        study: async ({studyId}, args, {db}) => {
+            const session = await db.session();
+            const [result] = await session.query('Studies').where({id: studyId}).all();
+            session.close();
+            return result;
+        },
+        biopsies: async ({id}, args, {db}) => {
+            const session = await db.session();
+            const result = await session.query('Biopsies').where({patientId: id}).all();
+            session.close();
+            return result;
+        }
+    },
+    Study: {
+        patients: async ({id}, args, {db}) => {
+            const session = await db.session();
+            const result = await session.query('Patients').where({studyId: id}).all();
+            session.close();
+            return result;
+        }
+    },
+    Biopsy: {
+        patient: async ({patientId}, args, {db}) => {
+            const session = await db.session();
+            const [result] = await session.query('Patients').where({id: patientId}).all();
+            session.close();
+            return result;
+        }
+    },
     Query: {
         getStudies: async (root, args, {db}) => {
             const session = await db.session();
