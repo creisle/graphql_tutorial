5c60ab67d8dbc3676c6f9f26923d8545b7dfa260
creisle <caralynreisle@gmail.com>
2019-10-09 09:14:14 -0700

    Use db connection in resolvers

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,11 +1,24 @@
-const data = require('./repo/data.json');
-
 const resolvers = {
     Query: {
-        getStudies: () => [...data.Studies],
-        getPatients: () => [...data.Patients],
-        getBiopsies: () => [...data.Biopsies]
-    }
+        getStudies: async (root, args, {db}) => {
+            const session = await db.session();
+            const result = await session.query('Studies').all();
+            session.close();
+            return result;
+        },
+        getPatients: async (root, args, {db}) => {
+            const session = await db.session();
+            const result = await session.query('Patients').all();
+            session.close();
+            return result;
+        },
+        getBiopsies: async (root, args, {db}) => {
+            const session = await db.session();
+            const result = await session.query('Biopsies').all();
+            session.close();
+            return result;
+        },
+    },
 }
 
 module.exports = resolvers;
