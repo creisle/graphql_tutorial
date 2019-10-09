9f40c532fb1badb7adf57feee97022201ffc00ad
creisle <caralynreisle@gmail.com>
2019-10-09 09:03:30 -0700

    Add initial query resolvers

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,1 +1,11 @@
-module.exports = {};
+const data = require('./repo/data.json');
+
+const resolvers = {
+    Query: {
+        getStudies: () => [...data.Studies],
+        getPatients: () => [...data.Patients],
+        getBiopsies: () => [...data.Biopsies]
+    }
+}
+
+module.exports = resolvers;
